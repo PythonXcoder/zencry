f8ba990ad5ee0ce44ed9cac5ea39b3848fd99b49
Snehashish Biswas <teamscenariox@gmail.com>
2022-09-28 15:46:56 +0530

    Bug Fix!

--- FallenRobot/__main__.py
+++ FallenRobot/__main__.py
@@ -219,21 +219,21 @@ def start(update: Update, context: CallbackContext):
         else:
             uptime = get_readable_time((time.time() - StartTime))
             m=update.effective_message
-	    c=update.effective_chat
+            c=update.effective_chat
             try:
-	        xD=m.reply_text(text="Hey {mention_html(user.id, html.escape(user.first_name))}!\n    how are you",chat_id=c.id, parse_mode=ParseMode.MARKDOWN)
-	        time.sleep(2)
-	        xD.edit_message_text(text="Let me prepare the start message for you", chat_id=c.id, parse_mode=ParseMode.MARKDOWM)
-	        time.sleep(2)
-	        xD.edit_message_text(text="🤍🤍", chat_id=c.id)
-	        time.sleep(2)
-	        xD.edit_message_text(text="🤍", chat_id=c.id)
-	        time.sleep(2)
-	        xD.delete()
-	        m.reply_sticker(sticker_id="CAACAgUAAx0CXBwX3QACIW5jMw5Y1H8ued1c7w32g5jUsUALxwACYQUAAnxKYVaPQeNelhOrpikE", chat_id=c.id)
-	        time.sleep(2)
+            	xD=m.reply_text(text="Hey {mention_html(user.id, html.escape(user.first_name))}!\n    how are you",chat_id=c.id, parse_mode=ParseMode.MARKDOWN)
+            	time.sleep(2)
+            	xD.edit_message_text(text="Let me prepare the start message for you", chat_id=c.id, parse_mode=ParseMode.MARKDOWM)
+            	time.sleep(2)
+            	xD.edit_message_text(text="🤍🤍", chat_id=c.id)
+            	time.sleep(2)
+            	xD.edit_message_text(text="🤍", chat_id=c.id)
+            	time.sleep(2)
+            	xD.delete()
+            	m.reply_sticker(sticker_id="CAACAgUAAx0CXBwX3QACIW5jMw5Y1H8ued1c7w32g5jUsUALxwACYQUAAnxKYVaPQeNelhOrpikE", chat_id=c.id)
+            	time.sleep(2)
             except Exception as e:
-	        LOGGER.debug(e)
+            	LOGGER.debug(e)
             else:
                 update.effective_message.reply_text(
                     PM_START_TEXT.format(dispatcher.bot.first_name, sql.num_users(), sql.num_chats(), uptime),
