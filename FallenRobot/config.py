542e682c3606c193df7c3f1cf77888a74a35f223
PythonXgamer <adityakjha@proton.me>
2022-12-02 18:03:31 +0530

    Update and rename sample_config.py to config.py

--- FallenRobot/config.py
+++ FallenRobot/config.py
@@ -14,17 +14,17 @@ class Config(object):
     # REQUIRED
     # Login to https://my.telegram.org and fill in these slots with the details given by it
 
-    API_ID = 123456  # integer value, dont use ""
-    API_HASH = "awoo"
-    TOKEN = "BOT_TOKEN"  # This var used to be API_KEY but it is now TOKEN, adjust accordingly.
-    OWNER_ID = 1356469075  # If you dont know, run the bot and do /id in your private chat with it, also an integer
-    OWNER_USERNAME = "anonymous_was_bot"
-    SUPPORT_CHAT = "DevilsHeavenMF"  # Your own group for support, do not add the @
+    API_ID = #123456  # integer value, dont use "15010241"
+    API_HASH = "dd21505ccb119c065519c3484ca161f2"
+    TOKEN = "5803537697:AAGvqVNJSI5-Hpik7OJDg_SCUHAXzVchW7o"  # This var used to be API_KEY but it is now TOKEN, adjust accordingly.
+    OWNER_ID = 5655841146  # If you dont know, run the bot and do /id in your private chat with it, also an integer
+    OWNER_USERNAME = "PythonXgamer"
+    SUPPORT_CHAT = "VIOLENCECHITCHAT"  # Your own group for support, do not add the @
     JOIN_LOGGER = (
-        -1001686672798
+        -1001800127388
     )  # Prints any new group the bot is added to, prints just the name and ID.
     EVENT_LOGS = (
-        -1001686672798
+        -1001800127388
     )  # Prints information like gbans, sudo promotes, AI enabled disable states that may help in debugging and shit
 
     # RECOMMENDED
